77e6579cc216ffb4096b856ab0972302080e4ed4
FunszeEvil <danielfoley1122@hotmail.com>
2020-02-05 16:45:06 +0000

    created BugZap.java class

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap extends PApplet
+{	
+	public void settings()
+	{
+		size(500, 500);
+	}
+
+	public void setup() {
+	}
+	
+
+}
